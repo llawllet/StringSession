07dc5db04c60045c92bb4cd3b15008ce85b9e9d6
Odi <85743359+llawllet@users.noreply.github.com>
2022-02-06 11:58:35 +0330

    -dead

--- stringsetup.py
+++ stringsetup.py
@@ -1,7 +1,7 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 #------------------------------------------#
-print("Online Telethon StringSessionGenerator")
+print("Telethon String Session Generator")
 
 APP_ID = int(input("Enter APP ID here: "))
 API_HASH = input("Enter API HASH here: ")
@@ -9,5 +9,5 @@ API_HASH = input("Enter API HASH here: ")
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
     string = client.session.save()
     result = client.send_message("me", string)
-    result.reply("Here is Your StringSession \nContact: @FeelDeD")
-    print("Check Your Telegram Saved Messages For StringSession")
+    result.reply("Here is Your String Session \nContact: @feelded")
+    print("Check Your Telegram Saved Messages For String Session")
